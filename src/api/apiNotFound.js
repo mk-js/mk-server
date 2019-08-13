fcdd3e69738f4477afaf2322290e399be5d6067a
lsg <lishengguo@qq.com>
2019-08-13 17:03:17 +0800

    notFoundAll

--- src/api/apiNotFound.js
+++ src/api/apiNotFound.js
@@ -1,13 +1,19 @@
 module.exports = (apiRootUrl, routes, services) => {
-    console.log("api 404 path: " + apiRootUrl + '/{p*}'); 
-    var apiNotFound = apiRootUrl + "/notFound"
-    var router = routes.filter(r=>r.path == apiNotFound)[0]
-    router && routes.push({
-        method: '*',
-        path: apiRootUrl + '/{p*}',
-        config: router.config
-    });
+    apiNotFound(apiRootUrl, apiRootUrl + '/notFound', routes)
+    apiNotFound('', apiRootUrl + '/notFoundAll', routes)
 
     return routes;
 }
- 
+function apiNotFound(apiRootUrl, apiNotFound, routes){
+  
+    var router = routes.filter(r=>r.path == apiNotFound)[0]
+    if(router){ 
+        console.log("api 404 path: " + apiRootUrl + '/{p*}'); 
+        routes.push({
+            method: 'POST',
+            path: apiRootUrl + '/{p*}',
+            config: router.config
+        }); 
+    }
+    return routes;
+}
